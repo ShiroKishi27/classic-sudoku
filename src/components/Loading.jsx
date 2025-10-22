4f67eec2f138d7dab99c046789541ecd5472aa85
ShiroKishi27 <heimslycalauagan@gmail.com>
2025-10-22 16:10:22 +0800

    Created a seperate file for readabilty

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Loading = () => {
+  return (
+    <div className="flex h-[496px] w-[823px] items-center justify-center rounded-lg bg-[#636363] text-2xl font-semibold text-white/80">
+      Grid Loading...
+    </div>
+  );
+};
+
+export default Loading;
