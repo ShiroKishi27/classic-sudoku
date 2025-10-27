990e2c5309892b508e151fc4a90cd63c31b6f7d4
ShiroKishi27 <heimslycalauagan@gmail.com>
2025-10-27 22:16:41 +0800

    Modified for better style

--- src/components/GameStatus.jsx
+++ src/components/GameStatus.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const GameStatus = ({ status, handleReset, handleNewGame }) => {
   return (
-    <div className="absolute top-1/2 left-1/2 mt-5 flex h-[532px] w-[346px] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center rounded-lg bg-white/70 text-center text-xl font-semibold text-gray-800 uppercase md:mt-[38px] md:h-[522px] md:w-[827px] md:text-2xl">
+    <div className="absolute top-1/2 left-1/2 mt-6.5 flex h-[502px] w-[346px] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center rounded-lg bg-white/70 text-center text-xl font-semibold text-gray-800 uppercase md:mt-[30px] md:h-[422px] md:w-[650px] md:text-2xl">
       {status}
       <div className="flex flex-row gap-3 text-sm text-white">
         <button
           onClick={handleReset}
-          className="bg-black/80 px-2 py-3.5 md:px-2.5 md:py-3"
+          className="cursor-pointer bg-black/80 px-2 py-3.5 md:px-2.5 md:py-3"
         >
           Reset
         </button>
         <button
           onClick={handleNewGame}
-          className="bg-black/80 px-2 py-3.5 md:px-2.5 md:py-3"
+          className="cursor-pointer bg-black/80 px-2 py-3.5 md:px-2.5 md:py-3"
         >
           New Game
         </button>
