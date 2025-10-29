2045759271e338785e8ed8ac6f445405433486e6
ShiroKishi27 <heimslycalauagan@gmail.com>
2025-10-29 23:48:52 +0800

    Modify the classes of buttons for better styling

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -32,24 +32,26 @@ const Controls = ({
           onClick={handlePencilMark}
           onMouseDown={(e) => e.preventDefault()}
           className={clsx(
-            "mx-1.5 cursor-pointer px-2.5 py-1.5 transition-all duration-300 ease-in hover:bg-[#636363] md:p-4",
-            isPencilMark ? "bg-white" : "",
+            "mx-1.5 flex h-14 w-14 cursor-pointer items-center justify-center !rounded-full p-1.25 transition-all duration-300 ease-in hover:bg-[#636363] md:p-4",
+            isPencilMark ? "!border !border-cyan-400" : "",
           )}
         >
-          <PencilLine className={clsx(isPencilMark ? "text-black/80" : "")} />
+          <PencilLine
+            className={clsx("h-5 w-5", isPencilMark ? "text-cyan-400" : "")}
+          />
         </button>
         <button
           onClick={handleReset}
-          className="mx-1.5 cursor-pointer px-2.5 py-1.5 transition-all duration-300 ease-in hover:bg-[#636363] md:p-4"
+          className="mx-1.5 flex h-14 w-14 cursor-pointer items-center justify-center !rounded-full p-1.25 transition-all duration-300 ease-in hover:bg-[#636363] md:p-4"
         >
-          <RotateCcw />
+          <RotateCcw className="h-5 w-5" />
         </button>
         <button
           onClick={handleErase}
           onMouseDown={(e) => e.preventDefault()}
-          className="mx-1.5 cursor-pointer px-2.5 py-1.5 transition-all duration-300 ease-in hover:bg-[#636363] md:p-4"
+          className="mx-1.5 flex h-14 w-14 cursor-pointer items-center justify-center !rounded-full p-1.25 transition-all duration-300 ease-in hover:bg-[#636363] md:p-4"
         >
-          <Eraser />
+          <Eraser className="h-5 w-5" />
         </button>
       </div>
       <button
@@ -59,39 +61,40 @@ const Controls = ({
         <Grid3x3 className="mr-2 text-sm md:text-2xl" /> New Game
       </button>
       <div className="mt-3 flex flex-row gap-1.75 md:grid md:grid-cols-3 md:grid-rows-3">
-        {Object.entries(numberCounts).map(([num, count]) => {
-          const isComplete = count === 9;
-          return (
-            <button
-              key={num}
-              onClick={() => handleClick(num)}
-              onMouseDown={(e) => e.preventDefault()}
-              disabled={isComplete}
-              className={clsx(
-                "flex aspect-square items-center justify-center bg-black/50 px-1 py-3.5 text-xl transition-all duration-300 ease-in hover:bg-[#636363] md:px-2.5 md:py-5",
-                isComplete
-                  ? "cursor-default bg-transparent opacity-50 hover:bg-transparent"
-                  : "cursor-pointer",
-              )}
-            >
-              {isComplete ? (
-                <Check
-                  className="animate-scale-in h-3 w-3 rounded-full bg-[#636363] p-1 text-white md:h-6 md:w-6"
-                  strokeWidth={3}
-                />
-              ) : (
-                <span
-                  className={clsx(
-                    "flex h-3 w-3 items-center justify-center md:h-6 md:w-6",
-                    isPencilMark ? "text-sm text-gray-500" : "text-blue-700/70",
-                  )}
-                >
-                  {num}
-                </span>
-              )}
-            </button>
-          );
-        })}
+        {numberCounts &&
+          Object.entries(numberCounts).map(([num, count]) => {
+            const isComplete = count === 9;
+            return (
+              <button
+                key={num}
+                onClick={() => handleClick(num)}
+                onMouseDown={(e) => e.preventDefault()}
+                disabled={isComplete}
+                className={clsx(
+                  "flex aspect-square items-center justify-center bg-black/50 px-1 py-3.5 text-xl transition-all duration-300 ease-in hover:bg-[#636363] md:px-2.5 md:py-5",
+                  isComplete
+                    ? "cursor-default bg-transparent opacity-50 hover:bg-transparent"
+                    : "cursor-pointer",
+                )}
+              >
+                {isComplete ? (
+                  <Check
+                    className="animate-scale-in h-3 w-3 rounded-full bg-[#636363] p-1 text-white md:h-6 md:w-6"
+                    strokeWidth={3}
+                  />
+                ) : (
+                  <span
+                    className={clsx(
+                      "flex h-3 w-3 items-center justify-center md:h-6 md:w-6",
+                      isPencilMark ? "text-sm text-gray-500" : "text-cyan-400",
+                    )}
+                  >
+                    {num}
+                  </span>
+                )}
+              </button>
+            );
+          })}
       </div>
     </div>
   );
