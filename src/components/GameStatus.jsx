e8758cc128e98d51a5dc3efd8b24a93abd947112
ShiroKishi27 <heimslycalauagan@gmail.com>
2025-10-30 18:35:22 +0800

    Adjusted div margin and height

--- src/components/GameStatus.jsx
+++ src/components/GameStatus.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const GameStatus = ({ status, handleReset, handleNewGame }) => {
   return (
-    <div className="absolute top-1/2 left-1/2 mt-6.5 flex h-[502px] w-[346px] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center rounded-lg bg-white/70 text-center text-xl font-semibold text-gray-800 uppercase md:mt-[30px] md:h-[422px] md:w-[650px] md:text-2xl">
+    <div className="absolute top-1/2 left-1/2 mt-5.5 flex h-[522px] w-[346px] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center rounded-lg bg-white/70 text-center text-xl font-semibold text-gray-800 uppercase md:mt-[30px] md:h-[422px] md:w-[650px] md:text-2xl">
       {status}
       <div className="flex flex-row gap-3 text-sm text-white">
         <button
