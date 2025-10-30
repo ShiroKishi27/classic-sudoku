40b5a87a7fc88a806eaf46f7252e394fa120b0d0
ShiroKishi27 <heimslycalauagan@gmail.com>
2025-10-30 18:37:53 +0800

    Changed div height and text size

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Loading = ({ error }) => {
   return error ? (
-    <div className="flex h-[532px] w-[346px] items-center justify-center rounded-lg bg-[#636363] text-2xl font-semibold text-white/80 uppercase md:h-[496px] md:w-[823px]">
+    <div className="flex h-[502px] w-[346px] items-center justify-center rounded-lg bg-[#636363] text-center text-xl font-semibold text-white/80 uppercase md:h-[422px] md:w-[650px] md:text-2xl">
       {error}
     </div>
   ) : (
-    <div className="flex h-[532px] w-[346px] items-center justify-center rounded-lg bg-[#636363] text-2xl font-semibold text-white/80 md:h-[496px] md:w-[823px]">
+    <div className="flex h-[502px] w-[346px] items-center justify-center rounded-lg bg-[#636363] text-xl font-semibold text-white/80 md:h-[422px] md:w-[650px] md:text-2xl">
       Grid Loading...
     </div>
   );
