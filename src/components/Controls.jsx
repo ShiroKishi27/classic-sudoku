b0b4e9dd07d5fb9ea13b84a473de322d28735340
ShiroKishi27 <heimslycalauagan@gmail.com>
2025-10-31 23:29:40 +0800

    Changed built-in class to custom

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -15,16 +15,12 @@ const Controls = ({
     <div className="mt-4 flex flex-col md:mt-0 md:ml-5">
       <div className="flex items-center justify-around text-xs">
         <p>
-          <span className="font-bold text-[rgb(182,182,182)]">
-            Difficulty:&nbsp;
-          </span>
-          {difficulty}
+          Difficulty:&nbsp;
+          <span className="text-clues font-bold">{difficulty}</span>
         </p>
         <p>
-          <span className="font-bold text-[rgb(182,182,182)]">
-            Mistakes:&nbsp;
-          </span>
-          {mistakeCount}/5
+          Mistakes:&nbsp;
+          <span className="text-clues font-bold">{mistakeCount}/5</span>
         </p>
       </div>
       <div className="my-1.5 flex items-stretch justify-center gap-2">
@@ -32,31 +28,31 @@ const Controls = ({
           onClick={handlePencilMark}
           onMouseDown={(e) => e.preventDefault()}
           className={clsx(
-            "mx-1.5 flex h-14 w-14 cursor-pointer items-center justify-center !rounded-full p-1.25 transition-all duration-300 ease-in hover:bg-[#636363] md:p-4",
-            isPencilMark ? "!border !border-cyan-400" : "",
+            "hover:bg-foreground mx-1.5 flex h-14 w-14 cursor-pointer items-center justify-center !rounded-full p-1.25 transition-all duration-300 ease-in md:p-4",
+            isPencilMark ? "!border-entry !border" : "",
           )}
         >
           <PencilLine
-            className={clsx("h-5 w-5", isPencilMark ? "text-cyan-400" : "")}
+            className={clsx("h-5 w-5", isPencilMark ? "text-entry" : "")}
           />
         </button>
         <button
           onClick={handleReset}
-          className="mx-1.5 flex h-14 w-14 cursor-pointer items-center justify-center !rounded-full p-1.25 transition-all duration-300 ease-in hover:bg-[#636363] md:p-4"
+          className="hover:bg-foreground mx-1.5 flex h-14 w-14 cursor-pointer items-center justify-center !rounded-full p-1.25 transition-all duration-300 ease-in md:p-4"
         >
           <RotateCcw className="h-5 w-5" />
         </button>
         <button
           onClick={handleErase}
           onMouseDown={(e) => e.preventDefault()}
-          className="mx-1.5 flex h-14 w-14 cursor-pointer items-center justify-center !rounded-full p-1.25 transition-all duration-300 ease-in hover:bg-[#636363] md:p-4"
+          className="hover:bg-foreground mx-1.5 flex h-14 w-14 cursor-pointer items-center justify-center !rounded-full p-1.25 transition-all duration-300 ease-in md:p-4"
         >
           <Eraser className="h-5 w-5" />
         </button>
       </div>
       <button
         onClick={handleNewGame}
-        className="flex cursor-pointer items-center justify-center bg-white px-2.5 py-1.5 font-bold text-black/80 transition-all duration-400 ease-out hover:bg-[#636363] hover:text-white md:px-2 md:py-3"
+        className="bg-new-game-secondary text-new-game-primary hover:bg-foreground hover:text-new-game-secondary flex cursor-pointer items-center justify-center px-2.5 py-1.5 font-bold transition-all duration-400 ease-out md:px-2 md:py-3"
       >
         <Grid3x3 className="mr-2 text-sm md:text-2xl" /> New Game
       </button>
@@ -71,7 +67,7 @@ const Controls = ({
                 onMouseDown={(e) => e.preventDefault()}
                 disabled={isComplete}
                 className={clsx(
-                  "flex aspect-square items-center justify-center bg-black/50 px-1 py-3.5 text-xl transition-all duration-300 ease-in hover:bg-[#636363] md:px-2.5 md:py-5",
+                  "bg-grid-btn hover:bg-foreground flex aspect-square items-center justify-center px-1 py-3.5 text-xl transition-all duration-300 ease-in md:px-2.5 md:py-5",
                   isComplete
                     ? "cursor-default bg-transparent opacity-50 hover:bg-transparent"
                     : "cursor-pointer",
@@ -79,14 +75,14 @@ const Controls = ({
               >
                 {isComplete ? (
                   <Check
-                    className="animate-scale-in h-3 w-3 rounded-full bg-[#636363] p-1 text-white md:h-6 md:w-6"
+                    className="animate-scale-in bg-foreground text-new-game-secondary h-3 w-3 rounded-full p-1 md:h-6 md:w-6"
                     strokeWidth={3}
                   />
                 ) : (
                   <span
                     className={clsx(
                       "flex h-3 w-3 items-center justify-center md:h-6 md:w-6",
-                      isPencilMark ? "text-sm text-gray-500" : "text-sky-400",
+                      isPencilMark ? "text-clues text-sm" : "text-entry",
                     )}
                   >
                     {num}
