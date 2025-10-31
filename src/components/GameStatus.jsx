017b36ba70bed1640445f34c00358620811cc83d
ShiroKishi27 <heimslycalauagan@gmail.com>
2025-10-31 23:40:40 +0800

    Changed built-in class to custom

--- src/components/GameStatus.jsx
+++ src/components/GameStatus.jsx
@@ -2,18 +2,18 @@ import React from "react";
 
 const GameStatus = ({ status, handleReset, handleNewGame }) => {
   return (
-    <div className="absolute top-1/2 left-1/2 mt-5.5 flex h-[522px] w-[346px] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center rounded-lg bg-white/70 text-center text-xl font-semibold text-gray-800 uppercase md:mt-[30px] md:h-[422px] md:w-[650px] md:text-2xl">
+    <div className="bg-new-game-secondary/70 text-new-game-primary absolute top-1/2 left-1/2 mt-5.5 flex h-[522px] w-[346px] -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center rounded-lg text-center text-xl font-semibold uppercase md:mt-[30px] md:h-[422px] md:w-[650px] md:text-2xl">
       {status}
-      <div className="flex flex-row gap-3 text-sm text-white">
+      <div className="text-new-game-secondary flex flex-row gap-3 text-sm">
         <button
           onClick={handleReset}
-          className="cursor-pointer bg-black/80 px-2 py-3.5 md:px-2.5 md:py-3"
+          className="bg-new-game-primary cursor-pointer px-2 py-3.5 md:px-2.5 md:py-3"
         >
           Reset
         </button>
         <button
           onClick={handleNewGame}
-          className="cursor-pointer bg-black/80 px-2 py-3.5 md:px-2.5 md:py-3"
+          className="bg-new-game-primary cursor-pointer px-2 py-3.5 md:px-2.5 md:py-3"
         >
           New Game
         </button>
--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Loading = ({ error }) => {
   return error ? (
-    <div className="flex h-[502px] w-[346px] items-center justify-center rounded-lg bg-[#636363] text-center text-xl font-semibold text-white/80 uppercase md:h-[422px] md:w-[650px] md:text-2xl">
+    <div className="bg-foreground text-new-game-secondary/80 flex h-[502px] w-[346px] items-center justify-center rounded-lg text-center text-xl font-semibold uppercase md:h-[422px] md:w-[650px] md:text-2xl">
       {error}
     </div>
   ) : (
-    <div className="flex h-[502px] w-[346px] items-center justify-center rounded-lg bg-[#636363] text-xl font-semibold text-white/80 md:h-[422px] md:w-[650px] md:text-2xl">
+    <div className="bg-foreground text-new-game-secondary/80 flex h-[502px] w-[346px] items-center justify-center rounded-lg text-xl font-semibold md:h-[422px] md:w-[650px] md:text-2xl">
       Grid Loading...
     </div>
   );
